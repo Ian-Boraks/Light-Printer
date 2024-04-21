0d0d1b1bdd9677ca21378e50de8cf0ab69551bf2
Ian Boraks <Ianboraks@gmail.com>
2024-04-21 12:08:27 -0400

    Updating gstream

--- tracker/src/main.py
+++ tracker/src/main.py
@@ -17,8 +17,8 @@ def open_camera() -> cv2.VideoCapture:
     # Define the GStreamer pipeline
     gst_pipeline = (
         "libcamerasrc ! "
-        "video/x-raw,format=YUV2,width=640,height=480,framerate=30/1 ! "
-        "videoconvert ! "
+        "video/x-raw,format=RGB,width=640,height=480,framerate=30/1 ! "
+        "videoconvert ! videoscale ! "
         "video/x-raw,format=BGR ! appsink"
     )
     
